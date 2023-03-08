250191c9aea2fca151cfac5e57c98dd4d31109cb
CrisSoven <20213tn044@utez.edu.mx>
2023-03-07 19:12:03 -0600

    mas arreglos responsivos

--- src/components/profile/PersonalInfoFormComponent.js
+++ src/components/profile/PersonalInfoFormComponent.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Colors from "../../utils/Colors";
 import { Icon } from "react-native-elements";
